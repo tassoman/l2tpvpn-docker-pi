32fb5b474f88c4d0eafb76c22e9be43e0a47dcc5
Tassoman <tassoman@gmail.com>
2020-10-31 20:01:58 +0100

    updated dockerSetup, by using apt

--- setupDocker.sh
+++ setupDocker.sh
@@ -2,17 +2,18 @@
 #
 echo $(date +"%F %T%z") "starting script setupDocker.sh"
 
-echo "----------------------------------"
-echo " INSTALLING DOCKER"
-echo "----------------------------------"
+echo "------------------------------------"
+echo " INSTALLING DOCKER on UBUNTU/DEBIAN "
+echo "------------------------------------"
 
 if [ -x "$(command -v docker)" ]; then
   echo " Docker is already installed"
 else
-  echo " Install docker"
-  curl -sSL https://get.docker.com | sh
+  echo " Install docker by apt"
+#  curl -sSL https://get.docker.com | sh
+  sudo apt install docker
   sudo systemctl enable docker
   sudo systemctl start docker
-  sudo usermod -aG docker pi
-  sudo su - pi
+  sudo usermod -aG docker ubuntu
+#  sudo su - ubuntu
 fi
